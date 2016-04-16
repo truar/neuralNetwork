4f95fcb06f2eac248e6fb7f147b3f082cea72d8d
truar <thibault.ruaro@gmail.com>
2016-04-16 20:37:30 +0200

    Commit de la base du projet

---
+++ NeuralNetwork/src/main/java/com/neural/network/NeuralNetwork/App.java
@@ -0,0 +1,13 @@
+package com.neural.network.NeuralNetwork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
